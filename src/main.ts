2f3355f7d420fd58e6b3d195a139c36d10237031
Oleksandr Yatsiuk <oleksandr.yatsiuk@gmail.com>
2021-04-30 18:21:02 +0300

    added cors configs

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filters/all-exceptions';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: {allowedHeaders:'Content-Type, Authorization, Accept, Origin'} });
 
   const options = new DocumentBuilder()
     .setTitle('PLC API')
